53a6f8a05512f6813715d704ed8e4e14c9317343
DalyaKablawi <160538849+DalyaKablawi@users.noreply.github.com>
2024-04-05 06:26:31 +0300

    added functions to delete tasks and called function to load page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const addTaskBtn = $("#formModal");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -9,9 +10,8 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-  const taskCard = $(
-    <div class="card text-center">
-      <div class="card-header" id="task-id">
+  const taskCard = $(`<div class="card text-center">
+<div class="card-header" id="task-id">
         ${task.id}
       </div>
       <div class="card-body">
@@ -25,9 +25,7 @@ function createTaskCard(task) {
           ${task.date}
         </p>
         <button class="btn btn-danger">Delete</button>
-      </div>
-    </div>
-  );
+      </div>`);
   return taskCard;
 }
 
@@ -46,13 +44,64 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  event.preventDefault();
+  const title = $("#task-title").val();
+  const description = $("#task-description").val();
+  const deadline = $("#task-date").val();
+
+  const newTask = {
+    id: generateTaskId(),
+    title: title,
+    description: description,
+    deadline: deadline,
+    status: "to-do",
+  };
+
+  taskList.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+
+  $("#formModal").modal("close");
+  renderTaskList();
+}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {}
+function handleDeleteTask(event) {
+  $(".card").remove();
+}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {}
+function handleDrop(event, ui) {
+  $(".card").droppable({
+    drop: function (event, ui) {
+      $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+    },
+  });
+}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {});
+$(document).ready(function () {
+  renderTaskList();
+
+  const form = $("#form").dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      Close: function () {
+        $(this).dialog("close");
+      },
+    },
+  });
+
+  addTaskBtn.on("click", function () {
+    $("#form").dialog("open");
+  });
+
+  $(function () {
+    $("#deadline").datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
+});

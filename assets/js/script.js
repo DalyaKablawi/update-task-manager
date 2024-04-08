ef468b98d1510ee00bfd2e3a53998600098e8ab1
DalyaKablawi <160538849+DalyaKablawi@users.noreply.github.com>
2024-04-08 19:45:13 +0300

    modified handleDrop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,12 +121,19 @@ function handleDeleteTask(event) {
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-  $(".card").droppable({
-    drop: function (event, ui) {
-      $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
-    },
-  });
+  const tasks = readTasksFromStorage();
+  const taskId = ui.draggable[0].dataset.taskId;
+  const newStatus = event.target.id;
+  for (let task of tasks) {
+    if (task.id === taskId) {
+      task.status = newStatus;
+    }
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTaskList();
 }
+
+
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
